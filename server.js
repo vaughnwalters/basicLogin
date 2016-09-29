a4e1ffcde7327392daeee225a86b2ec7fefa09db
Vaughn Walters <vaughnwalters@gmail.com>
2016-09-29 12:42:06 -0500

    getting ready for deploy to heroku

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+  'use strict';
 
 const express = require('express');
 const session = require('express-session');
@@ -25,7 +25,7 @@ app.use(session({
 
 app.use((req, res, next) => {
   console.log("req.session", req.session );
-  app.locals.email = req.session.email
+  app.locals.email = req.session && req.session.email
   next()
 })
 app.use(express.static('public'))
